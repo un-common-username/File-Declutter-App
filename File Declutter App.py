54831b537dc1b69303e8049bfcbf9bb4b4fa06ab
Abdul Majed <52102347+un-common-username@users.noreply.github.com>
2020-04-11 22:05:03 +0530

    Update File Declutter App.py

--- File Declutter App.py
+++ File Declutter App.py
@@ -7,6 +7,6 @@ mydir='<CHANGE THIS TO YOUR WORKING DIRECTORY>'
 os.chdir(mydir)
 for root_path,sub_dir,files in os.walk(os.getcwd()):
     for i in files:
-        if not 'en.srt' in str(i) and '.srt' in str(i) :
+        if '.srt' in str(i) :
             os.remove(os.path.join(root_path,str(i)))
             
